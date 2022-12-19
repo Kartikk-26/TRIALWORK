a4a4b78244173098a537131ff8aca32ab4c9b957
KARTIK JAIN <115203884+Kartikk-26@users.noreply.github.com>
2022-12-19 15:39:13 +0530

    Add files via upload

---
+++ reversestring.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+
+
+    char a[100];
+    printf("ENTER THE STRING \n");
+    gets(a);
+    strrev(a);
+    printf("STRING IN REVERSE IS %s",a);
+
+}
